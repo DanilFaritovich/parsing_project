a08d574baf707ce0fe6c048a1831f0c4f17c8baf
miftakhov_df <miftakhov_df@taximaxim.ru>
2022-02-10 14:20:13 +0300

    Оптимизирован поиск конечных ссылок

--- main.py
+++ main.py
@@ -23,61 +23,34 @@ class SearchStarterPages(object):
 
 
 class SearchLinks(object):
-    def __init__(self, text, url):
-        self.list_of_links = []
-        self.text = text
-        self.url = url
-        self.img_link = None
-
-    def start_links(self):
+    def links(self, text, url, orientation):
+        list_of_links, img_link = [], None
         for len_of_list in range(0, 10):
             # find img
-            for link in self.text.xpath('.//a/@href'):
-                if link[-4:] == ".png" or link[-4:] == ".jpg":
-                    self.img_link = link
-                    break
+            img_link = [link
+                        for link in text.xpath('.//a/@href')
+                        if link[-4:] == ".png" or link[-4:] == ".jpg"]
 
             # save link and img link
-            self.list_of_links.append([self.url, self.img_link])
+            list_of_links.append([url, img_link[0]])
 
             # find next page
-            next_page = self.text.xpath('.//a[text() = "Next >"]/@href')
-            self.url = 'https://xkcd.com{}'.format(next_page[0])
-            r = requests.get(self.url, headers=headers)
-            self.text = html.fromstring(r.text)
-
-        return self.list_of_links
-
-    def end_links(self):
-        for len_of_list in range(0, 10):
-            # find img
-            for link in self.text.xpath('.//a/@href'):
-                if link[-4:] == ".png" or link[-4:] == ".jpg":
-                    self.img_link = link
-                    break
-
-            # save link and img link
-            self.list_of_links.append([self.url, self.img_link])
-
-            # find next page
-            next_page = self.text.xpath('.//a[text() = "< Prev"]/@href')
-            self.url = 'https://xkcd.com{}'.format(next_page[0])
-            r = requests.get(self.url, headers=headers)
-            self.text = html.fromstring(r.text)
-        return self.list_of_links
+            next_page = text.xpath('.//a[text() = "{}"]/@href'.format(orientation))
+            url = 'https://xkcd.com{}'.format(next_page[0])
+            r = requests.get(url, headers=headers)
+            text = html.fromstring(r.text)
+        return list_of_links
 
 
 if __name__ == '__main__':
     # search start and end url and html
     starter_page = SearchStarterPages()
-    start_page, end_page = starter_page.search("|<"), starter_page.search(">|")  # (html, url)
+    page = [starter_page.search("|<"), starter_page.search(">|")]  # [[html, url], [html, url]]
 
     # search start and last links of img and link
-    object_start_links = SearchLinks(start_page[0], start_page[1])
-    object_end_links = SearchLinks(end_page[0], end_page[1])
-    list_start_links = object_start_links.start_links()
-    list_end_links = object_end_links.end_links()
-
+    object_links = SearchLinks()
+    list_links = [object_links.links(page[0][0], page[0][-1], "Next >"), object_links.links(page[-1][0], page[-1][1], "< Prev")]
+    
     with open("result.txt", "w") as w:
         w.writelines("10 start links\n")
         for i in list_start_links:
